569ac883a9544502cf4c37a5c55aeec2c5c52010
outadoc <outatime@caramail.com>
2011-10-27 12:16:02 +0200

    La rétrocompatibilité, c'est le bien

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,13 @@
 function getItem(key, defaultReturn)
 {
-    var value = window.localStorage.getItem(key);
-    
-    if(value == null)
-    {
-    	value = defaultReturn;
-    }
+	try {
+	    var value = window.localStorage.getItem(key);
+	    if(value == null)
+	    	value = defaultReturn;
+    } catch(e) {
+		console.log('Error inside getItem');
+		console.log(e);
+	}
     
 	return value;
 }
@@ -19,4 +21,27 @@ function setItem(key, value)
 		console.log('Error inside setItem');
 		console.log(e);
 	}
+}
+
+function removeItem(key)
+{
+	try {
+		window.localStorage.removeItem(key);
+	} catch(e) {
+		console.log('Error inside removeItem');
+		console.log(e);
+	}
+}
+
+//retro-compatibility check
+var code = getItem('code', null);
+var hd = getItem('hd', null);
+
+if(code != null && hd != null)
+{
+	setItem('profile1.code', code);
+	setItem('profile1.hd', hd.replace('hd', ''));
+	
+	removeItem('code');
+	removeItem('hd');
 }
